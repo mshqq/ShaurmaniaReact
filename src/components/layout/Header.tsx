008a47c69914d26cbef6267bb03ec2fc7b6da62b
mshqq <miahasik@gmail.com>
2026-07-23 17:23:20 +0900

    feat: Header и Button переписаны с точки зрения концепции default mobile-first
    Убран лишний фрагмент в Header, добавлены брейкпоинты для логотипа и
    отступов. В Button добавлена иконка корзины на мобильных экранах
    (текст скрыт через sr-only, показывается с sm)

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -3,16 +3,15 @@ import logo from './../../assets/logo.svg';
 
 export default function Header() {
   return (
-    <>
-      <div className="bg-[#E73F22]">
-        <div className="container mx-auto flex flex-row items-center justify-between py-3">
-          <img
-            src={logo}
-            alt=""
-          />
-          <Button text={'Корзина'} />
-        </div>
+    <div className="bg-[#E73F22]">
+      <div className="container mx-auto flex flex-row items-center justify-around lg:justify-between py-3 lg:px-0">
+        <img
+          className="h-8 sm:h-12"
+          src={logo}
+          alt="Shaurmania"
+        />
+        <Button text={'Корзина'} />
       </div>
-    </>
+    </div>
   );
 }
--- src/components/ui/Button.tsx
+++ src/components/ui/Button.tsx
@@ -6,10 +6,11 @@ interface ButtonProps {
 export function Button({ text, link }: ButtonProps) {
   return (
     <a
-      className="bg-[#F9D236] text-black rounded-md font-bold text-lg py-2 px-3 hover:bg-amber-400 transition-colors delay-75 ease-in cursor-pointer"
+      className="bg-[#F9D236] text-black rounded-md font-bold text-sm px-2.5 py-1.5 hover:bg-amber-400 transition-colors delay-75 ease-in cursor-pointer sm:text-lg sm:py-2 sm:px-3"
       href={link ?? '#'}
     >
-      {text}
+      <span className="text-xl sm:hidden">🛒</span>
+      <span className="sr-only sm:not-sr-only">{text}</span>
     </a>
   );
 }
